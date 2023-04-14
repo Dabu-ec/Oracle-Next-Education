391fe34f25b66a9d3ff842dde67972842a3b3c88
DannyJoel05 <dannyjoel98@hotmail.com>
2023-04-14 10:50:41 -0500

    funcion desencriptar

--- encriptador/app/js/maindb.js
+++ encriptador/app/js/maindb.js
@@ -16,3 +16,15 @@ function encriptar() {
 }
 
 
+function desencriptar() {
+    var resultado = txt_input.value
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    document.getElementById("cmp_empty").style.display = "none";
+    txt_ouput.innerText = resultado;
+}
+
+
