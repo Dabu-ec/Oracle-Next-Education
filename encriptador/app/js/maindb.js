3036cc3980ba5da8e4136b42bd5944f5fe048ad5
DannyJoel05 <dannyjoel98@hotmail.com>
2023-04-14 10:47:18 -0500

    funcion encriptar

--- encriptador/app/js/maindb.js
+++ encriptador/app/js/maindb.js
@@ -1,6 +1,18 @@
 console.log('DANNY JOEL BUCAY SHUCAD')
 
 
+var txt_input = document.getElementById('txt_input');
 
+
+function encriptar() {
+    var resultado = txt_input.value
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    document.getElementById("cmp_empty").style.display = "none";
+    txt_ouput.innerText = resultado;
+}
 
 
---
+++ encriptador/srs/elementos-pagina.md
@@ -0,0 +1,10 @@
+## Elementos principales de la página
+
+- [ ] Título de su sitio web
+- [ ] Campo para el texto que va a ser encriptado/desencriptado
+- [ ] Un botón para encriptar
+- [ ] Un botón para desencriptar
+- [ ] Área para mostrar el texto encriptado/desencriptado.
+
+### Extra!
+- [ ] Un rodapie  con los datos de la persona que desarrolló el sitio web!
